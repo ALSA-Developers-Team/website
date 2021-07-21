e9057eae2247759bc8e2cab97dd58fdd660f3f79
aldo1411 <62414829+aldo1411@users.noreply.github.com>
2021-07-20 20:34:24 -0500

    producción v2

--- js/main.js
+++ js/main.js
@@ -10,9 +10,28 @@
   scaleMobile: 1.00,
   color: 0xDEF2F1,
   backgroundColor: 0x17252A,
-  points: 20.00,
-  maxDistance: 18.00,
-  spacing: 15.50
+  points: window.innerWidth > 568 ? 20 : 25,
+  maxDistance: window.innerWidth > 568 ? 18 : 22,
+  spacing: window.innerWidth > 568 ? 15.50 : 30.50
   });
+
+  const swiper = new Swiper('.swiper-container', {
+    speed: 600,
+        parallax: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+    },
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    mousewheelControl: true
+});
+
 
 
